325b0cd6c52e1ba3cac9d4c120d76725f7956711
Learning-howto-Code <jaketorohopkins@icloud.com>
2026-05-23 11:35:05 -0700

    broadcsting works

--- backend/advertise.py
+++ backend/advertise.py
@@ -1,18 +1,20 @@
 import asyncio
 from bless import BlessServer
+from bless.backends.characteristic import GATTCharacteristicProperties
+from bless.backends.characteristic import GATTAttributePermissions
 
 MESH_SERVICE_UUID = "12345678-1234-5678-1234-56789abcdef0"
 MESH_CHAR_UUID = "12345678-1234-5678-1234-56789abcdef1"
 
 async def advertise():
-    server = BlessServer(name="shree-macbook")
+    server = BlessServer(name="alice-macbook")
     await server.add_new_service(MESH_SERVICE_UUID)
     await server.add_new_characteristic(
         MESH_SERVICE_UUID,
         MESH_CHAR_UUID,
-        properties=0x02 | 0x08,  # read | write
+        properties=GATTCharacteristicProperties.read,
+        permissions=GATTAttributePermissions.readable,
         value=bytearray(b"hello"),
-        permissions=0x01 | 0x02
     )
     await server.start()
     print("Advertising...")
